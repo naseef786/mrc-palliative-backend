edc45743644997e1dcee5db3eb4010a45b199ffd
naseef786 <naseefac786@gmail.com>
2026-01-27 12:40:40 +0400

    updated analytics module features

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,7 @@ import volunteerRoutes from "./routes/volunteer.routes";
 import scheduleRoutes from "./routes/schedule.routes";
 import analyticsRoutes from "./routes/analytics.routes";
 import { connectDB } from "./config/db";
+import { error } from "node:console";
 
 const app = express();
 
@@ -17,6 +18,8 @@ app.use(async (req, res, next) => {
         await connectDB();
         next();
     } catch (err) {
+        console.log(error);
+
         res.status(500).json({ error: "Database connection failed" });
     }
 });
--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,6 @@ dotenv.config();
 
 import app from "./app";
 
-
 app.listen(process.env.PORT, () =>
   console.log(`Server running on port ${process.env.PORT}`)
 );
